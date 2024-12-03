50f304e4737aec91fcc289e5e84b002cde644e88
Sumedha Singh Rathor <130545882+SumedhaSinghRathor@users.noreply.github.com>
2024-12-03 22:11:01 +0530

    Add files via upload

--- Expense_Tracker/script.js
+++ Expense_Tracker/script.js
@@ -1,20 +1,55 @@
-function updateClock() {
-  let now = new Date();
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
-  let seconds = now.getSeconds();
+const addExpenseBtn = document.querySelector(".add-expense-btn");
+const expenseList = document.querySelector(".expense-list");
+const totalExpenses = document.querySelector(".total-expenses h3");
 
-  hours = hours < 10 ? "0" + hours : hours;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
+let expenses = [];
+let total = 0;
 
-  const hoursElement = document.getElementById("hours");
-  const minutesElement = document.getElementById("minutes");
-  const secondsElement = document.getElementById("seconds");
+function renderExpenses() {
+  let html = "";
+  expenses.forEach((expense, index) => {
+    html += `
+        <div class="expense-item" data-index="${index}">
+        <div class="expense-item-description">${expense.description}</div>
+        <div class="expense-item-amount">$${expense.amount.toFixed(2)}</div>
+        <button class="delete-expense-btn">&times;</button>
+        </div>
+    `;
+  });
 
-  hoursElement.textContent = hours;
-  minutesElement.textContent = minutes;
-  secondsElement.textContent = seconds;
+  expenseList.innerHTML = html;
+  totalExpenses.innerText = `Total Expenses: $${total.toFixed(2)}`;
 }
 
-setInterval(updateClock, 1000);
+function addExpense() {
+  const description = prompt("Enter Expense Description: ");
+  const amount = parseFloat(prompt("Enter Expense Amount: "));
+
+  if (description && !isNaN(amount)) {
+    const expense = {
+      description: description,
+      amount: amount,
+    };
+
+    expenses.push(expense);
+    total += amount;
+    renderExpenses();
+  } else {
+    alert("Invalid expense amount. Please enter a valid number.");
+  }
+}
+
+addExpenseBtn.addEventListener("click", addExpense);
+
+function deleteExpense(index) {
+  total -= expenses[index].amount;
+  expenses.splice(index, 1);
+  renderExpenses();
+}
+
+expenseList.addEventListener("click", function (event) {
+  if (event.target.classList.contains("delete-expense-btn")) {
+    const index = event.target.closest(".expense-item").dataset.index;
+    deleteExpense(index);
+  }
+});
